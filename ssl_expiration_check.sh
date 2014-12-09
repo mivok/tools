0bdb8ce4aeb0b9affbf941e58cece7be67be7d95
Mark Harrison <mark@mivok.net>
2014-12-09 13:05:12 -0500

    Script to dump SSL cert expiration dates for websites quickly

---
+++ ssl_expiration_check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Quick script to print out expiration dates of SSL certificates for websites
+#
+# Usage:
+# ./ssl_expiration_check.sh www.example.com www.example2.com www.example3.com
+
+for h in "$@"; do
+    echo -n "$h: "
+    echo | openssl s_client -connect "$h":443 2>&1 | \
+        openssl x509 -noout -dates | grep notAfter | awk -F= '{print $2}'
+done
