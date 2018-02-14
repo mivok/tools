0ff32ed622bbfa40a70c363d8bdde9d8702baf4b
Mark Harrison <mark@mivok.net>
2018-02-14 10:54:36 -0500

    Make yaml_to_json use python3

--- misc/yaml_to_json.py
+++ misc/yaml_to_json.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Takes in yaml, dumps it out as json
 import json
 import sys
@@ -7,7 +7,7 @@ import yaml
 fh = open(sys.argv[1])
 try:
     data = yaml.load(fh)
-except yaml.error.YAMLError, e:
-    print e
+except yaml.error.YAMLError as e:
+    print(e)
     sys.exit(1)
-print json.dumps(data, indent=4, sort_keys=True)
+print(json.dumps(data, indent=4, sort_keys=True))
