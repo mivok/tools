7137240ce44e5f570998da5cbe86681384d0d938
Mark Harrison <mark@mivok.net>
2019-03-03 20:52:55 -0500

    Make aws_instance_search much smarter
    It can take a public or private IP address, instance ID, or instance
    name and will automatically do the right thing in most cases.
    
    aws_identify_instance is now no longer needed as a result.

--- aws/aws_instance_search.sh
+++ aws/aws_instance_search.sh
@@ -1,28 +1,50 @@
 #!/bin/bash
-# Search for instances matching a given name
-REGION=
-PROFILE=
+# Search for instances matching given criteria
 EXACTMATCH=
+REVERSE_IP_DISPLAY=
+FILTERKEY=
 
 usage() {
     echo "Usage: $0 [-r REGION] [-p PROFILE] PATTERN"
     echo
-    echo "  -r REGION  -- Specify AWS region (Default: $REGION)"
-    echo "  -p PROFILE -- Specify credentials profile (Default: $PROFILE)"
-    echo "  -x         -- Only return exact matches (Default: partial)"
-    exit 1
+    echo "Easily search for aws instances"
+    echo
+    echo "  -r REGION    -- Specify AWS region (Default: $AWS_DEFAULT_REGION)"
+    echo "  -p PROFILE   -- Specify credentials profile (Default: $AWS_PROFILE)"
+    echo "  -f FILTERKEY -- What to filter on (Default: auto)"
+    echo "  -i           -- Display Public IP Address (Default: private)"
+    echo "  -x           -- Only return exact matches (Default: partial)"
+    echo
+    echo "Run 'aws ec2 describe instances help' to view available filter keys"
+    echo
+    echo "Examples: tag:Name, tag:Environment, ip-address,"
+    echo "private-ip-address, availability-zone"
+    echo
+    echo "'auto' will detect IP addresses (rfc1918 for private IPs) or instance"
+    echo "IDs beginning with 'i-', and will search the name tag otherwise"
+    echo
+    echo "When searching by public IP, the public IP will be displayed by"
+    echo "default, and the meaning of -i is reversed."
 }
 
-while getopts ":p:r:x" opt; do
+while getopts ":f:hip:r:x" opt; do
     case $opt in
-        r)  REGION="$OPTARG"
+        f)  FILTERKEY="$OPTARG"
             ;;
-        p)  PROFILE="$OPTARG"
+        p)  export AWS_PROFILE="$OPTARG"
+            ;;
+        i)  REVERSE_IP_DISPLAY=1
+            ;;
+        r)  export AWS_DEFAULT_REGION="$OPTARG"
             ;;
         x)  EXACTMATCH=1
             ;;
+        h)  usage
+            exit
+            ;;
         *)  echo "Invalid option -- '$OPTARG'"
             usage
+            exit 1
             ;;
     esac
 done
@@ -32,26 +54,36 @@ PATTERN="$1"
 
 [[ -z $1 ]] && usage
 
+# Decide what to search for based on input heuristic
+if [[ -z $FILTERKEY || $FILTERKEY == "auto" ]]; then
+    FILTERKEY="tag:Name"
+
+    if [[ $PATTERN =~ ^i- ]]; then
+        FILTERKEY="instance-id"
+    elif [[ $PATTERN =~ ^(172\.(1[6-9]|2[0-9]|3[0-1])\.|10\.|192\.168\.) ]]; then
+        FILTERKEY="private-ip-address"
+    elif [[ $PATTERN =~ [0-9.] ]]; then
+        FILTERKEY="ip-address"
+    fi
+fi
+
 if [[ -z $EXACTMATCH ]]; then
     PATTERN="*$PATTERN*"
 fi
 
-OPTS=()
-if [[ -n $PROFILE ]]; then
-    OPTS+=(--profile "$PROFILE")
+IPATTR="PrivateIpAddress"
+if [[ (-n $REVERSE_IP_DISPLAY && $FILTERKEY != "ip-address") ||
+      (-z $REVERSE_IP_DISPLAY && $FILTERKEY == "ip-address") ]]; then
+    IPATTR="PublicIpAddress"
 fi
 
-if [[ -n $REGION ]]; then
-    OPTS+=(--region "$REGION")
-fi
-
-aws "${OPTS[@]}" ec2 describe-instances \
-    --filters "Name=tag:Name,Values=$PATTERN" | \
+aws ec2 describe-instances \
+    --filters "Name=$FILTERKEY,Values=$PATTERN" | \
     jq -r '[
         .Reservations[].Instances[] |
             [
                 .InstanceId,
-                .PrivateIpAddress,
+                .'"$IPATTR"',
                 [.Tags[] | select(.Key == "Name") | .Value][0]
             ]
         ] | sort_by(.[2]) | .[] | @tsv'
