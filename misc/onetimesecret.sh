6ab19dacbee322676f3f7b541b193f515088cea1
Mark Harrison <mark@mivok.net>
2020-01-07 13:51:05 -0800

    Allow specifying a secret on the command line (for testing)

--- misc/onetimesecret.sh
+++ misc/onetimesecret.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-# Creates a one time secret from stdin using onetimesecret.com and displays
+# Creates a one time secret using onetimesecret.com and displays
 # the URL to the secret.
-curl -s -F 'secret=<-' -F 'ttl=604800' \
+if [[ -n $1 ]]; then
+    SECRET="$*"
+else
+    SECRET="<-" # Curl magic to read from stdin
+fi
+
+curl -s -F "secret=$SECRET" -F 'ttl=604800' \
     https://onetimesecret.com/api/v1/share | \
     jq -r '"https://onetimesecret.com/secret/\(.secret_key)"'
