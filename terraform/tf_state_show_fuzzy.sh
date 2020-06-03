946a9046542a0337a3e9e0aa4a422a5fc26cda8c
Mark Harrison <mark@mivok.net>
2020-06-03 15:31:26 -0400

    Typo fix

--- terraform/tf_state_show_fuzzy.sh
+++ terraform/tf_state_show_fuzzy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # List all resources in the current terraform state, filter then through fzf,
 # and show the state for the selected resource.
-RESOURCE=$(terrraform state list | fzf -q "$1" -1 -0)
+RESOURCE=$(terraform state list | fzf -q "$1" -1 -0)
 if [[ -n "$RESOURCE" ]]; then
     terraform state show "$RESOURCE"
 else
