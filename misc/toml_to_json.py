1ab4e03a0925deb57a7eafab2043d55f480988f1
Mark Harrison <mark@mivok.net>
2020-07-23 13:55:38 -0400

    Switch toml to json script to python 3

--- misc/toml_to_json.py
+++ misc/toml_to_json.py
@@ -1,14 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Takes in toml, dumps it out as json
 # Run pip install toml to install the toml module
 import json
 import sys
 import toml
 
-fh = open(sys.argv[1])
+if len(sys.argv) > 1:
+    fh = open(sys.argv[1])
+else:
+    fh = sys.stdin
+
 try:
     data = toml.load(fh)
-except toml.TomlDecodeError, e:
-    print e
+except toml.TomlDecodeError as e:
+    print(e)
     sys.exit(1)
-print json.dumps(data, indent=4, sort_keys=True)
+print(json.dumps(data, indent=4, sort_keys=True))
