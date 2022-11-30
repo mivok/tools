d56ea7d96a946fc4ce58d9962696c63199b2e6ac
Mark Harrison <mark@mivok.net>
2022-11-30 18:03:11 -0500

    Add stow_adopt script

---
+++ misc/stow_adopt.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Adopt a file with GNU stow
+#
+# This script moves a real file into the appropriate location inside a GNU stow
+# managed directory, and then re-runs stow to create the symlink again. Useful
+# for moving a file inside your dotfiles repo.
+SOURCE="$1"
+PKG="$2"
+
+if [[ -z "$SOURCE" || -z "$PKG" ]]; then
+    echo "Usage: $0 SOURCE_FILE PKG"
+    echo
+    echo "Adopts a file that isn't already there into a repo managed by gnu stow"
+    echo
+    echo "SOURCE_FILE should point to the file to adopt"
+    echo "PKG should point to a directory inside the current directory"
+    exit 1
+fi
+
+ABSOLUTE_SOURCE="$(cd "$(dirname "$SOURCE")" && pwd)/$(basename "$SOURCE")"
+DEST_DIR="${PKG}/${ABSOLUTE_SOURCE#"$HOME"/}" # Source file relative to home dir
+
+# Make sure the destination directory exists
+echo "=> Making directory $DEST_DIR"
+mkdir -p "$DEST_DIR"
+echo "=> Moving $SOURCE to $DEST_DIR"
+mv "$SOURCE" "$DEST_DIR"
+echo "=> Rerunning stow on $PKG"
+stow -v "$PKG"
